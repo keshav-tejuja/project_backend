70ca35a10afdfa82664a79c881cdc3c6f8cffcec
Keshav <keshavkt01@gmail.com>
2025-07-11 16:55:20 +0530

    added util code

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ dotevn.config({
     path:"./.env"
 });
 connectDB()
+.then(()=>{{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on Port ${process.env.PORT}`)
+    })
+}})
+.catch((error)=>{
+    console.log("Error connecting to MongoDB:",error)
+    }
+)
 
 
 
